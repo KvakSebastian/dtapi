ffff2f4d938f5f98dac679fe12a373f38749ba1d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-27 09:53:05 +0000

    Deploy KvakSebastian/dtapi to github.com/KvakSebastian/dtapi.git:gh-pages

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    BASEURL: 'http://dtapi.if.ua:8000/api/',
-    defaultImage:
-        'https://jeffjbutler.com/wp-content/uploads/2018/01/default-user.png',
-};
